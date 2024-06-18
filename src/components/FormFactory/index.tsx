d01bd8ed06b54e5f0ba0b4eab2b3be99707d529c
Durgesh2607 <dk829445@gmail.com>
2024-06-18 23:34:25 +0530

    refactor: form fields

--- src/components/FormFactory/index.tsx
+++ src/components/FormFactory/index.tsx
@@ -6,7 +6,13 @@ const componentMap: {
   Header: (content) => <h2 className="text-xl font-semibold">{content}</h2>,
   TextBlock: (content) => <p>{content}</p>,
   Image: (content) => (
-    <img height={250} width={250} src={content} alt="image" className="rounded-xl" />
+    <img
+      height={250}
+      width={250}
+      src={content}
+      alt="image"
+      className="rounded-xl"
+    />
   ),
   Footer: (content) => <footer>{content}</footer>,
 };
